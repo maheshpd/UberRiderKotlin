001c039041151f1a69c3c01d2a55e8c7d1284082
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-04-03 20:06:50 +0530

    New Uber Clone #11 Rider App Register User

---
+++ app/src/main/java/com/createsapp/uberriderkotlin/common/Common.kt
@@ -0,0 +1,8 @@
+package com.createsapp.uberriderkotlin.common
+
+import com.createsapp.uberriderkotlin.model.RiderModel
+
+object Common {
+    var currentRider: RiderModel? = null
+    const val RIDER_INFO_REFERENCE: String = "Riders"
+}
---
+++ app/src/main/java/com/createsapp/uberriderkotlin/model/RiderModel.kt
@@ -0,0 +1,8 @@
+package com.createsapp.uberriderkotlin.model
+
+class RiderModel {
+    var firstName: String=""
+    var lastName:String=""
+    var phoneNumber:String=""
+    var avatar:String=""
+}
